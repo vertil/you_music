72f59014cf27a5a540db05c0192475580e9211bc
unknown <vertil98@gmail.com>
2025-01-23 20:43:15 +0300

    aboba

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
 import yt_dlp
 import json
+import sys
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    #гоч
-    video_url = "https://www.youtube.com/watch?v=_LDA3OX3Ww4&list=PLfQAxVHE2OPKHbleRw1iKWisdzgmST9_w&index=1"
-    #ваг
-    #video_url = "https://www.youtube.com/playlist?list=PLfQAxVHE2OPI0_bAPpwnUO3LlSUhOc64r"
+    # VIDEO DOWLOAD
+    # sss="https://www.youtube.com/playlist?list=PL6Nx1KDcurkBdxssD1k56SDnwuTuX2bBr"
+    # ydl_opts={
+    #     'format': 'mkv/bestvideo/best',
+    #     'paths': {"temp": "1c lessons/buff/", 'home': "1c lessons/buff/"},
+    #     'ignoreerrors': True
+    # }
+    #
+    # with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    #     ydl.download(sss)
+    #
+    # sys.exit()
 
-    #video_url = "https://www.youtube.com/watch?v=mtbGja6z-Y4&list=WL&index=1"
-
+    #AUDIO DOWLOAD
+    video_url = "https://youtu.be/3vOu4OgRT1w?si=IYqScKTNOoT-IDSl"
     ydl_opts = {
         'format': 'm4a/bestaudio/best',
         'ffmpeg_location': 'C:\\ffmpeg\\bin',
-        #'outtmpl': 'tracks/гоч',
-        'paths': {"temp": "tracks/гоч", 'home': "tracks/гоч"},
-        # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
-        'postprocessors': [{  # Extract audio using ffmpeg
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-        }],
-        'ignoreerrors': False
-    }
-
-    ydl_opts = {
-        'format': 'm4a/bestaudio/best',
-        'ffmpeg_location': 'C:\\ffmpeg\\bin',
+        'paths': {"temp": "tracks/buff/", 'home': "tracks/buff/"},
         'postprocessors': [{  # Extract audio using ffmpeg
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
@@ -34,12 +31,10 @@ if __name__ == '__main__':
         'ignoreerrors': True
     }
 
-    # with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    #     info = ydl.extract_info(video_url, download=False)
-    #     print(json.dumps(ydl.sanitize_info(info)))
+
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
 
-        err_code =  ydl.download(video_url)
+        err_code = ydl.download(video_url)
 
 
